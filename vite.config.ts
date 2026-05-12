890cc19866b25040feff95816c061837d66160c1
komed3 <pk97@outlook.de>
2026-05-12 22:58:14 +0200

    get build no (commit hash)

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import { resolve } from 'path';
+import { execSync } from 'node:child_process';
+import { resolve } from 'node:path';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+const build = execSync( 'git rev-parse --short HEAD' ).toString().trim();
+
 export default defineConfig( () => ( {
   plugins: [ react(), tailwindcss() ],
-  resolve: { alias: { '@': resolve( __dirname, '.' ) } }
+  resolve: { alias: { '@': resolve( __dirname, '.' ) } },
+  define: { 'process.env.build': JSON.stringify( build ) }
 } ) );
