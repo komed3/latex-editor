c9255526567530c7a8a216ed2b4f104ebec7064e
komed3 <pk97@outlook.de>
2026-05-12 23:43:10 +0200

    Create src/hook/useEditor.ts

---
+++ src/hook/useEditor.ts
@@ -0,0 +1,6 @@
+import { toPng } from 'html-to-image';
+import jsPDF from 'jspdf';
+import LZString from 'lz-string';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+
+export const useEditor = () => {};
