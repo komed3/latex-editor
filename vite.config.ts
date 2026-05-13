4eafbf514fe148d92758ee4bd47873f4a4cabc78
komed3 <pk97@outlook.de>
2026-05-13 02:30:57 +0200

    chunks won't work with prismjs

--- vite.config.ts
+++ vite.config.ts
@@ -11,18 +11,5 @@ export default defineConfig( () => ( {
   plugins: [ react(), tailwindcss() ],
   resolve: { alias: { '@': resolve( __dirname, '.' ) } },
   define: { 'process.env.build': JSON.stringify( build ) },
-  build: {
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: { output: { manualChunks( id: string ) {
-      if ( id.includes( 'node_modules' ) ) {
-        if ( id.includes( 'html-to-image' ) ) return 'html2img';
-        if ( id.includes( 'jspdf' ) ) return 'jspdf';
-        if ( id.includes( 'katex' ) ) return 'katex';
-        if ( id.includes( 'lucide' ) ) return 'icons';
-        if ( id.includes( 'prismjs' ) ) return 'prismjs';
-        if ( id.includes( 'react' ) || id.includes( 'react-dom' ) ) return 'react';
-        return 'vendor';
-      }
-    } } }
-  }
+  build: { chunkSizeWarningLimit: 9999 }
 } ) );
